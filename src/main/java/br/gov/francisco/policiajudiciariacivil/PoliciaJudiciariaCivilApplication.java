f5f3d7e45d63fee42093c356e7c6aeb7ba82ae88
Francisco Sousa <franciscoeds1987@gmail.com>
2023-09-08 18:31:07 -0300

    projeto criado

---
+++ src/main/java/br/gov/francisco/policiajudiciariacivil/PoliciaJudiciariaCivilApplication.java
@@ -0,0 +1,13 @@
+package br.gov.francisco.policiajudiciariacivil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliciaJudiciariaCivilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliciaJudiciariaCivilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/francisco/policiajudiciariacivil/PoliciaJudiciariaCivilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.francisco.policiajudiciariacivil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliciaJudiciariaCivilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
