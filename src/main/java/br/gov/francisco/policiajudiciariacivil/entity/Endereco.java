88ed95336242c006eca6dcc0cf0ceed4e3e290fa
Francisco Sousa <franciscoeds1987@gmail.com>
2023-09-09 21:12:37 -0300

    entidade Endereco criada e mapeada

---
+++ src/main/java/br/gov/francisco/policiajudiciariacivil/entity/Endereco.java
@@ -0,0 +1,47 @@
+package br.gov.francisco.policiajudiciariacivil.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "endereco")
+@Entity
+public class Endereco implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @SequenceGenerator(
+            name = "endereco_seq",
+            sequenceName = "endereco_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "endereco_seq"
+    )
+    @Column(name = "end_id")
+    private Integer id;
+
+    @Column(name = "end_tipo_logradouro", nullable = false)
+    private String tipoLogradouro;
+
+    @Column(name = "end_logradouro", nullable = false)
+    private String logradouro;
+
+    @Column(name = "end_numero", nullable = false)
+    private Integer numero;
+
+    @Column(name = "end_bairro", nullable = false)
+    private String bairro;
+
+}
