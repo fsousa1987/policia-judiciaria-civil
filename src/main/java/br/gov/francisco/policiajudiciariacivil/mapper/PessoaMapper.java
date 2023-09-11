36dac1c9169eca2344ddc48de5b21c13b543e44d
Francisco Sousa <franciscoeds1987@gmail.com>
2023-09-10 22:48:26 -0300

    interface PessoaMapper criada

---
+++ src/main/java/br/gov/francisco/policiajudiciariacivil/mapper/PessoaMapper.java
@@ -0,0 +1,14 @@
+package br.gov.francisco.policiajudiciariacivil.mapper;
+
+import br.gov.francisco.policiajudiciariacivil.entity.Pessoa;
+import br.gov.francisco.policiajudiciariacivil.response.PessoaResponse;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PessoaMapper {
+
+    List<PessoaResponse> toPessoaResponse(List<Pessoa> pessoa);
+
+}
